3c60344e8987ead18e3f814aadd207463b593099
ifl <iflstore@gmail.com>
2019-07-25 20:07:26 +0300

    +useragent

--- lesson3/homework3.py
+++ lesson3/homework3.py
@@ -10,4 +10,42 @@ job.ru или hh.ru. (Можно с обоих сразу) Приложение
 
 import requests
 from lxml import html
+from fake_useragent import UserAgent
+
+print(UserAgent())
+
+def request_to_site():
+    site = r'https://hh.ru/search/vacancy'
+    params = {
+        'text':'',
+        'area':'1',
+        'salary':'',
+        'currency_code':'RUR',
+        'experience':'doesNotMatter',
+        'order_by':'relevance',
+        'search_period':'',
+        'items_on_page':'20',
+        'no_magic':'true'
+        }
+    
+    try:
+        request = requests.get(
+            site, 
+            headers={'User-Agent': UserAgent().chrome}, 
+            params=params)
+        root = html.fromstring(request.text)
+        #//a[contains(@class,"bloko-link HH-LinkModifier")]
+
+        result_list = root.xpath('.//a[contains(@class,"bloko-link HH-LinkModifier")]/@href')
+        if result_list:
+            for i in result_list:
+                print(i)
+        else:
+            print('At your request no result found')
+            print(request.text)
+    except Exception as err:
+        print(f'Error: {err}')
+
+if __name__ == '__main__':
+    request_to_site()
 
